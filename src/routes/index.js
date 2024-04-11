feaa74cd163cc0a47993c242415e6a4c2cc3eb5d
Niharika <niharika0108@gmail.com>
2024-04-11 11:35:57 +0200

    format file + TODO message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
-import Weather from "../pages/Weather";
-import WeatherDetails from "../pages/Weather/Details";
-import NumberGame from "../pages/NumberGame";
-import History from '../pages/NumberGame/History';
+//TODO remove this file if not in use
+
+import { createBrowserRouter } from 'react-router-dom';
 import { URL } from '../helpers/constants';
 import PublicLayout from '../layouts/PublicLayout';
-import TodoList from "../pages/TodoList";
-import Calculator from "../pages/Calculator";
-import Ecommerce from "../pages/Ecommerce";
+import Calculator from '../pages/Calculator';
+import Ecommerce from '../pages/Ecommerce';
+import Home from '../pages/Home';
+import NumberGame from '../pages/NumberGame';
+import History from '../pages/NumberGame/History';
+import TodoList from '../pages/TodoList';
+import Weather from '../pages/Weather';
+import WeatherDetails from '../pages/Weather/Details';
 const {
   HOME,
   NUMBER_HISTORY,
@@ -17,42 +19,79 @@ const {
   WEATHER_DETAILS,
   TODO_LIST,
   CALCULATOR,
-  ECOMMERCE
+  ECOMMERCE,
 } = URL;
 
-const routes = createBrowserRouter([
+const routes = createBrowserRouter(
+  [
+    {
+      path: HOME,
+      element: (
+        <PublicLayout>
+          <Home />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: WEATHER,
+      element: (
+        <PublicLayout>
+          <Weather />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: WEATHER_DETAILS,
+      element: (
+        <PublicLayout>
+          <WeatherDetails />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: NUMBER_DASHBOARD,
+      element: (
+        <PublicLayout>
+          <NumberGame />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: NUMBER_HISTORY,
+      element: (
+        <PublicLayout>
+          <History />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: TODO_LIST,
+      element: (
+        <PublicLayout>
+          <TodoList />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: CALCULATOR,
+      element: (
+        <PublicLayout>
+          <Calculator />
+        </PublicLayout>
+      ),
+    },
+    {
+      path: ECOMMERCE,
+      element: (
+        <PublicLayout>
+          <Ecommerce />
+        </PublicLayout>
+      ),
+    },
+  ],
   {
-    path: HOME,
-    element: <PublicLayout><Home /></PublicLayout>,
+    basename: '/portfolio',
   },
-  {
-    path: WEATHER,
-    element: <PublicLayout><Weather /></PublicLayout>,
-  },
-  {
-    path: WEATHER_DETAILS,
-    element: <PublicLayout><WeatherDetails /></PublicLayout>,
-  },
-  {
-    path: NUMBER_DASHBOARD,
-    element: <PublicLayout><NumberGame /></PublicLayout>,
-  },
-  {
-    path: NUMBER_HISTORY,
-    element: <PublicLayout><History /></PublicLayout>,
-  },
-  {
-    path: TODO_LIST,
-    element: <PublicLayout><TodoList /></PublicLayout>,
-  },
-  {
-    path: CALCULATOR,
-    element: <PublicLayout><Calculator /></PublicLayout>,
-  },
-  {
-    path: ECOMMERCE,
-    element: <PublicLayout><Ecommerce /></PublicLayout>,
-  },
-], { basename: '/portfolio' });
+);
 
 export default routes;
